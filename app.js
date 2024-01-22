942718d7c2c549495e02ec17bbaa6ffb2700b555
huzfm <huzaaifmushtaq@gmail.com>
2024-01-22 11:51:23 +0530

    skills

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const navbar = document.querySelector('.menu');
 hamburger.addEventListener('click',function(){
       hamburger.classList.toggle('active');
       navbar.classList.toggle('active');
-})
+});
+// const navLinks = document.querySelectorAll('nav-links');
+// const windowPathName = window.location.pathname;
+// navLinks.forEach(navLinks =>{
+//       if(navLinks.href.includes(windowPathName)){
+//             navLinks.classList.add('active');
+//       }
+// });
+ 
